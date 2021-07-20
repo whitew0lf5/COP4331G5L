b5b2ebb8fc9223c39a3d394186c3c1ee55a6da9b
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2021-07-20 16:00:53 -0400

    Update LoginScreen.js

--- App/screens/LoginScreen.js
+++ App/screens/LoginScreen.js
@@ -1,8 +1,9 @@
 import React, { Component, useState } from 'react';
-import { Button, ImageBackground ,View, Text, TextInput,Dimensions} from 'react-native';
+import { Button, ImageBackground ,View, Text, TextInput,Dimensions, Linking} from 'react-native';
 import Overlay from 'react-native-modal-overlay';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import axios from 'axios';
+import emailjs from 'emailjs-com';
 
 const image = { uri: "https://cdn1.vectorstock.com/i/1000x1000/74/05/pokemon-go-logo-icon-vector-23237405.jpg" };
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
@@ -12,17 +13,37 @@ global.password = '';
 global.registerName = '';
 global.registerPassword = '';
 global.registerSecondPassword = '';
+global.email1 = '';
+global.email2 = '';
 global.res = -1;
+global.forgotName = '';
 
 export default class Homescreen extends Component 
 { 
-    onClose = () => this.setState({modalVisible: false});
+    onClose = () => 
+    {  
+        this.setState({modalVisible: false});
+        this.setState({message: ''});
+    }
     toggleOverlayOpen = () => this.setState({modalVisible: true});
     toggleOverlayClose = () => this.setState({modalVisible: false});
 
-    onCloseR = () => this.setState({registerVisible: false});
+    onCloseR = () => 
+    {
+        this.setState({registerVisible: false});
+        this.setState({registerMessage: ''});
+    }
     toggleOverlayOpenR = () => this.setState({registerVisible: true});
     toggleOverlayCloseR = () => this.setState({registerVisible: false});
+
+    onCloseF = () => 
+    {
+        this.setState({forgotVisible: false});
+        this.setState({forgotMessage: ''});
+    }
+
+    toggleOverlayOpenF = () => this.setState({forgotVisible: true});
+    toggleOverlayCloseF = () => this.setState({forgotVisible: false});
 
     constructor()   
     {    
@@ -31,8 +52,10 @@ export default class Homescreen extends Component
         {       
             message: "",
             registerMessage: "",
+            forgotMessage: "",
             modalVisible: false,
-            registerVisible: false  
+            registerVisible: false,
+            forgotVisible: false 
         }  
     }
 
@@ -79,17 +102,49 @@ export default class Homescreen extends Component
         <View style={{ flexDirection:'row' }}>          
         <Text style={{fontSize:20, color: 'black', backgroundColor : 'transparent'}}>Re-enter Password: </Text>          
         <TextInput style={{height: 30,fontSize:20, backgroundColor:'transparent', color : 'black'}}  placeholder="Password" secureTextEntry={true} onChangeText={(val) => { this.changePass2Handler(val) }}/>  
+        </View>
+
+        <View style={{ flexDirection:'row' }}>          
+        <Text style={{fontSize:20, color: 'black', backgroundColor : 'transparent'}}>Email: </Text>          
+        <TextInput style={{height: 30,fontSize:20, backgroundColor:'transparent', color : 'black'}}  placeholder="Email" onChangeText={(val) => { this.changeEmailHandler(val) }}/>  
         </View> 
 
         </View> 
         <Button title="Register" onPress={this.handleRegister}/>
         <Text style={{fontSize:15, backgroundColor: 'transparent', color: 'black'}}>{this.state.registerMessage}</Text>
         </Overlay>
+
+        <Overlay visible={this.state.forgotVisible} onClose={this.onCloseF} closeOnTouchOutside>
+        <ImageBackground source={{uri:'https://image.shutterstock.com/image-vector/hi-there-inscription-handwritten-lettering-260nw-1798736197.jpg',}}style={{flex: 1}}/>
+         
+        <View style={{alignItems: 'flex-end', alignItems: 'center'}}>        
+        <View style={{ flexDirection:'row' }}>          
+        <Text style={{fontSize:20, color: 'black', backgroundColor : 'transparent'}}>Username: </Text>          
+        <TextInput style={{height: 30,fontSize:20, backgroundColor:'transparent',color: 'black'}}  placeholder="Username" onChangeText={(val) => { this.changeForgotNameHandler(val) }}  />                
+        </View>        
+        
+        </View> 
+        <Button title="Reset Password" onPress={this.handleResetPassword}/>
+        <Text style={{fontSize:15, backgroundColor: 'transparent', color: 'black'}}>{this.state.forgotMessage}</Text>
+        </Overlay>
         
         </View>
 
         <Button title="Login" onPress={this.toggleOverlayOpen}/>
-        <Button title="Register" onPress={this.toggleOverlayOpenR}/>        
+        <Button title="Register" onPress={this.toggleOverlayOpenR}/>
+        <View style={{width: '100%',
+        height: 50,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute', //Here is the trick
+        bottom: 0, //Here is the trick}}>
+        }}>
+        <Text style={{color: 'blue', textAlign: "center",}}
+            onPress={this.toggleOverlayOpenF}>
+        Forgot Password?
+        </Text>
+        </View>       
         </ImageBackground>
             
         </View>    
@@ -110,12 +165,16 @@ export default class Homescreen extends Component
                 },
             });
 
-
-            if(JSON.stringify(response.status) == 200)
+            if(!response.data.verified)
+            {
+                this.setState({message: "User not yet verified"});
+            }
+            else
             {
                 this.setState({modalVisible: false});
                 this.setState({message: "" });
                 this.props.navigation.navigate('Card');
+                
             }
 
               
@@ -158,11 +217,21 @@ export default class Homescreen extends Component
                 params: 
                 {
                         username: global.registerName,
+                        email: global.email1,
                         password: global.registerPassword,
                 },
             })
             .then(function (response) 
              {
+                emailjs.send(
+                    'dream5Team',
+                    'template_y5so3r9',
+                    {
+                        username: global.registerName,
+                        to: global.email1,
+                    },
+                    'user_K6vGRZGvw7nrh6PmSSw3N'
+                );
                 global.res = JSON.stringify(response.status);
             })
             .catch(function (error) 
@@ -196,4 +265,59 @@ export default class Homescreen extends Component
     {    
         global.registerSecondPassword = val;  
     }
+
+    changeEmailHandler = async (val) =>  
+    {    
+        global.email1 = val;  
+    }    
+    
+    changeEmail2Handler = async (val) =>  
+    {
+        global.email2 = val;  
+    }
+
+    changeForgotNameHandler = async (val) =>  
+    {
+        global.forgotName = val;  
+    }
+
+    handleResetPassword = async () =>   
+    {    
+        try    
+        {      
+            var obj = {login:global.loginName.trim(),password:global.password.trim()};     
+            var js = JSON.stringify(obj);  
+            const response = await axios.get('http://198.199.67.109:3000/api/email', {
+                params: {
+                    username: global.forgotName,
+                },
+            });
+
+            const email = response.data.email;
+
+            if(response.status == 200)
+            {
+                this.setState({forgotVisible: false});
+                this.setState({forgotMessage: ''});   
+            }
+
+            emailjs.send(
+                'dream5Team',
+                'template_8kxjp6j',
+                {
+                    username: global.forgotName,
+                    to: email,
+                },
+                'user_K6vGRZGvw7nrh6PmSSw3N'
+            );
+
+            
+
+              
+        }    
+        catch(e)   
+        {
+            this.setState({forgotMessage: "User does not exist" });
+        }  
+    }   
 }   
