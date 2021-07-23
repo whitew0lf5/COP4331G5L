fa7a01b4318f493cd2dbaa6a4028d34a3b08c2c6
Nathanto.ai <nto@knights.ucf.edu>
2021-07-23 09:26:01 -0400

    I've tired many different methods and a belive the map() function should work for transferring the json sets over but I could not get it to work.

--- App/screens/SetsScreen.js
+++ App/screens/SetsScreen.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import { Button, View, Text, ImageBackground, Dimensions } from 'react-native';
+import { Button, View, Text, ImageBackground, Dimensions, TouchableOpacity, StyleSheet } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Overlay from 'react-native-modal-overlay';
+import sets from '../data/sets.json';
+
 
 const image = { uri: "https://www.teahub.io/photos/full/16-165638_cool-dark-pokemon-backgrounds.jpg" };
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+//const set = require('../data/sets.json')
 
-
-export default class Cardscreen extends Component 
+class Cardscreen extends Component 
 {  
+    constructor()   
+    {    
+        super()    
+        this.state =     
+        {       
+            data: () => ({
+                setsData: sets.data,
+                height: 50,
+                mySets:{},
+                userName: "",
+            })
+        }  
+    }
     render() 
     {
         return (      
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>       
-        <ImageBackground source={image} resizeMode="cover" style={{ flex: 1, justifyContent:"center", width: viewportWidth, height : viewportHeight}}>
-        <View>
-        
-        <Button title="To Login" onPress={() => this.props.navigation.navigate('Login')}/>  
-        
-        </View> 
-        </ImageBackground>   
-        </View>    )  
+            <View>
+            {
+               this.state.data.map((item) => (
+                  <TouchableOpacity
+                     key = {item.id}
+                     style = {styles.container}>
+                     <Text style = {styles.text}>
+                        {item.name}
+                     </Text>
+                  </TouchableOpacity>
+               ))
+            }
+            </View>
+        )
     }
 }
+export default Cardscreen
+
+const styles = StyleSheet.create ({
+   container: {
+      padding: 10,
+      marginTop: 3,
+      backgroundColor: '#d9f9b1',
+      alignItems: 'center',
+   },
+   text: {
+      color: '#4f603c'
+   }
+})
